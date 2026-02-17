4925ed5b6370bbbfccbbd30c02609bbacd5e21cb
Yury <YuryB_work@proton.me>
2026-02-17 19:09:46 +0300

    роуты страниц

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ Route::get('/', function () {
     return view('/pages/home/index');
 });
 
-Route::get('/menu', function () {
-    return view('pages/menu/index'); // вернет файл resources/views/menu.blade.php
-})->name('menu.index');
+//Route::get('/menu', function () {
+//    return view('pages/menu/index'); // вернет файл resources/views/menu.blade.php
+//})->name('menu.index');
+
+Route::view('/menu', 'pages.menu.index')->name('menu');
+
+Route::view('/cart', 'pages.cart.index');
+
+Route::middleware('auth')->group(function () {
+    Route::view('/profile', 'pages.profile.index')->name('profile');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/auth', function () {
+        return view('pages.auth.index');
+    })->name('auth');
+});
+
